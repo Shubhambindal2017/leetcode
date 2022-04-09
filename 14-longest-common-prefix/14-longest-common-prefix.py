af4875450cc35c532b0abe31ecf634e11c91d850
Shubham Bindal <bindals610@gmail.com>
2022-04-09 20:15:12 +0530

    Time: 55 ms (37.39%), Space: 14.1 MB (13.46%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -1,5 +1,7 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        '''
+        # case 1 - O(l * nlogn)
         if len(strs) == 1:
             return strs[0]
         smallest_string_lexographic = min(strs)
@@ -16,5 +18,23 @@ class Solution:
             else:
                 return common_prefix
         return common_prefix
-            
+        '''
+        # case 2 - O(l * n) - O(S)
+        if len(strs) == 1:
+            return strs[0]
+        string1 = strs[0]
+        for i in range(1, len(strs)):
+            string2 = strs[i]
+            min_length = min(len(string1), len(string2))
+            common_prefix = ''
+            for j in range(min_length):
+                if string1[j] == string2[j]:
+                    common_prefix += string1[j]
+                else:
+                    break
+            if len(common_prefix) == 0:
+                return ''
+            else:
+                string1 = common_prefix
+        return common_prefix
         
