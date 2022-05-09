ce8c0565e1d37fbdaebde510b430d0116da0a4c6
Shubham Bindal <bindals610@gmail.com>
2022-05-09 22:38:44 +0530

    Time: 60 ms (15.20%), Space: 13.9 MB (71.90%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,21 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        array = [0] * (10**4)
-        current_index = 0
+        array = []
         for el in s:
-            if el == '[':
-                array[current_index] = ']'
-                current_index += 1
-            elif el == '{':
-                array[current_index] = '}'
-                current_index += 1
-            elif el == '(':
-                array[current_index] = ')'
-                current_index += 1
+            if (el == '['): 
+                array.append(']')
+            elif (el == '('): 
+                array.append(')')
+            elif (el == '{'): 
+                array.append('}')
             else:
-                if array[current_index-1] != el:
-                    return False
+                if len(array) > 0:
+                    popped_el = array.pop()
+                    if popped_el != el:
+                        return False
                 else:
-                    current_index -= 1
-        return True if current_index == 0 else False
+                    return False
+        return array == []
         
